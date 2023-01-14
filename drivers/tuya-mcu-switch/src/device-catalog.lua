b2bfc2eb3fe33aa8d8aa106fb6a6ae0fab577060
Inseong <67549204+insd47@users.noreply.github.com>
2023-01-15 02:27:33 +0900

    fix: find_child error

--- drivers/tuya-mcu-switch/src/device-catalog.lua
+++ drivers/tuya-mcu-switch/src/device-catalog.lua
@@ -1,16 +1,16 @@
 local deviceCatalog = {
-    _TZE200_1n2kyphz_TS0601 = {
-        gangs = 4,
-        dataPoints = {"\x01", "\x02", "\x03", "\x04"}
-    },
-    _TZE200_9mahtqtg_TS0601 = {
-        gangs = 6,
-        dataPoints = {"\x01", "\x02", "\x03", "\x04", "\x05", "\x06"}
-    },
-    _TZE200_r731zlxk_TS0601 = {
-        gangs = 6,
-        dataPoints = {"\x01", "\x02", "\x03", "\x04", "\x05", "\x06"}
-    }
+  ["_TZE200_1n2kyphz/TS0601"] = {
+    gangs = 4,
+    dataPoints = {"\x01", "\x02", "\x03", "\x04"}
+  },
+  ["_TZE200_9mahtqtg/TS0601"] = {
+    gangs = 6,
+    dataPoints = {"\x01", "\x02", "\x03", "\x04", "\x05", "\x06"}
+  },
+  ["_TZE200_r731zlxk/TS0601"] = {
+    gangs = 6,
+    dataPoints = {"\x01", "\x02", "\x03", "\x04", "\x05", "\x06"}
+  }
 }
 
 return deviceCatalog
